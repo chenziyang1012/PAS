9a625d8b443d9a1eec1f34b87718eee2749a2640
chenziyang1012 <czy76811211@163.com>
2026-07-08 21:49:47 +0800

    feat: replace el-image preview with custom dialog that closes on outside click

---
+++ frontend/src/components/PreviewImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="display:inline-block;cursor:pointer" @click="open=true">
+    <img :src="src" style="width:56px;height:56px;object-fit:cover;border-radius:4px" />
+    <el-dialog v-model="open" :show-header="false" width="fit-content" :close-on-click-modal="true" append-to-body center>
+      <img :src="src" style="max-width:90vw;max-height:85vh;display:block" />
+    </el-dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+defineProps<{ src: string }>()
+const open = ref(false)
+</script>
+
+<style>
+.el-dialog .el-dialog__body:has(> img) { padding: 0; line-height: 0; }
+</style>
